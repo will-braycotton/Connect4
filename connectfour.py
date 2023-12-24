d1a2f626f29c1644e09cb5a679342e0c262077c4
nero-z <willbraycotton@gmail.com>
2023-12-24 15:16:57 -0500

    Comment

---
+++ connectfour.py
@@ -0,0 +1,29 @@
+menu = 'Welcome to Connect Four\n1: Single Player\n2: Two Player\nX: Exit'
+user_input = input(menu)
+
+
+
+def print_board(board):
+    for i in range(0,6):
+        board.append(x)
+    for i in board:
+        for x in i:
+            print(x, end=' ')
+        print()
+    for i in range(0,7):
+        print(i, end=" ")
+'''
+def win_(board):
+'''
+'''
+while user_input != "X":
+    x = ["_"]*7
+    board=[]        
+    if user_input == "2":
+        win = False
+        while win ==  False:
+            print('e')
+            
+
+            user_input = "X"
+'''
